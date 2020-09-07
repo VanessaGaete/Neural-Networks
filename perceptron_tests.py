96c90cc29566654c1b5b8a5c7b77e37550431dc0
VanessaGaete <48606635+VanessaGaete@users.noreply.github.com>
2020-09-07 16:22:59 -0300

    Tests for perceptron activity added

---
+++ perceptron_tests.py
@@ -0,0 +1,28 @@
+from perceptron import *
+import unittest
+
+#tests
+class TestPerceptron(unittest.TestCase):
+  def test_and(self):
+    self.assertAlmostEqual(PerceptronAND().output(0,0), 0)
+    self.assertAlmostEqual(PerceptronAND().output(0,1), 0)
+    self.assertAlmostEqual(PerceptronAND().output(1,0), 0)
+    self.assertAlmostEqual(PerceptronAND().output(1,1), 1)
+
+  def test_or(self):
+    self.assertAlmostEqual(PerceptronOR().output(0,0), 0)
+    self.assertAlmostEqual(PerceptronOR().output(0,1), 1)
+    self.assertAlmostEqual(PerceptronOR().output(1,0), 1)
+    self.assertAlmostEqual(PerceptronOR().output(1,1), 1)
+
+  def test_NAND(self):
+    self.assertAlmostEqual(PerceptronOR().output(0,0), 1)
+    self.assertAlmostEqual(PerceptronOR().output(0,1), 1)
+    self.assertAlmostEqual(PerceptronOR().output(1,0), 1)
+    self.assertAlmostEqual(PerceptronOR().output(1,1), 0)
+
+  def test_NAND(self):
+    self.assertAlmostEqual(SummingGate(1,1).output(), 0,1)
+    self.assertAlmostEqual(SummingGate(0,0).output(), 0,0)
+    self.assertAlmostEqual(SummingGate(0,1).output(), 1,0)
+    self.assertAlmostEqual(SummingGate(1,0).output(), 1,0)
